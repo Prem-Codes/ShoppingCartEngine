dd53a085216b6f2fdccf55a2df3245649f97d1ee
Prem Raj <PXRA@novonordisk.com>
2021-10-12 00:35:18 +0200

    Implementation to handle new changes in code.

--- ShoppingCartEngine/ShoppingCartEngine/Controller/PaymentController.cs
+++ ShoppingCartEngine/ShoppingCartEngine/Controller/PaymentController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShoppingCartEngine.Handlers;
 using ShoppingCartEngine.Models;
 
 namespace ShoppingCartEngine.Controller
@@ -9,69 +10,38 @@ namespace ShoppingCartEngine.Controller
     {
         public Product HandlePayment(ProductTypes type, string itemName="")
         {
-            if (type == ProductTypes.BOOK || type == ProductTypes.OTHER)
+            Product product;
+
+            switch (type)
             {
-                return HandlePaymentforPhysicalProduct(type, itemName);
-            }
-            else
-            {
-                return HandlePaymentforNonPhysicalProduct(type, itemName);
-            }
-        }
-
-        private Product HandlePaymentforPhysicalProduct(ProductTypes type, string itemName)
-        {
-            Product product = new Product {ActionMessages = new List<string> {"Generated Packing Slip."}};
-
-            Console.WriteLine("Generated Packing Slip.");
-            
-            product.ActionMessages.Add("Commission Payment to the Agent.");
-            Console.WriteLine("Commission Payment to the Agent.");
-
-            if (type == ProductTypes.BOOK)
-            {
-                product.ProductName = string.IsNullOrEmpty(itemName) ? itemName : String.Empty;
-                product.ActionMessages.Add("Created a duplicate slip for the royalty department.");
-                Console.WriteLine("Created a duplicate slip for the royalty department.");
-            }
-
-            product.ProductType = type;
-            return product;
-        }
-
-        private Product HandlePaymentforNonPhysicalProduct(ProductTypes type, string itemName)
-        {
-            Product product = new Product {ActionMessages = new List<string>()};
-
-            if (type == ProductTypes.VIDEO)
-            {
-                if (itemName.ToLower().Contains("learning to ski."))
+                case ProductTypes.BOOK:
                 {
-                    product.ProductName = string.IsNullOrEmpty(itemName) ? itemName : String.Empty;
-                    product.ActionMessages.Add("You get a free First Aid Video.");
-                    Console.WriteLine("You get a free First Aid Video.");
+                    product = new BookPaymentHandler(itemName);
+                    break;
+                }
+                case ProductTypes.MEMBERSHIP:
+                {
+                    product = new MembershipPaymentHandler();
+                    break;
+                }
+                case ProductTypes.UPGRADE:
+                {
+                    product = new UpgradePaymentHandler();
+                    break;
+                }
+                case ProductTypes.VIDEO:
+                {
+                    product = new VideoPaymentHandler(itemName);
+                    break;
+                }
+                case ProductTypes.OTHER:
+                default:
+                {
+                    product = new OtherPaymentHandler();
+                    break;
                 }
             }
 
-            if (type == ProductTypes.MEMBERSHIP)
-            {
-                product.ActionMessages.Add("Membership Activated.");
-                Console.WriteLine("Membership Activated.");
-            }
-
-            if (type == ProductTypes.UPGRADE)
-            {
-                product.ActionMessages.Add("Upgrade to the membership applied.");
-                Console.WriteLine("Upgrade to the membership applied.");
-            }
-
-            if (type == ProductTypes.MEMBERSHIP || type == ProductTypes.UPGRADE)
-            {
-                product.ActionMessages.Add($"email regarding the {type.ToString().ToLower()} sent to user.");
-                Console.WriteLine($"email regarding the {type.ToString().ToLower()} sent to user.");
-            }
-
-            product.ProductType = type;
             return product;
         }
     }
--- ShoppingCartEngine/ShoppingCartEngine/Program.cs
+++ ShoppingCartEngine/ShoppingCartEngine/Program.cs
@@ -8,30 +8,27 @@ namespace ShoppingCartEngine
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            var paymentController = new PaymentController();
+            string itemName = "";
+
+            foreach (var value in Enum.GetValues(typeof(ProductTypes)))
             {
-                var paymentController = new PaymentController();
-                string itemName = "";
-
-                foreach (var value in Enum.GetValues(typeof(ProductTypes)))
-                {
-                    Console.WriteLine("{0,3}   {1}",
-                        (int)value, ((ProductTypes)value));
-                }
-
-                Console.WriteLine("Enter the Name of the product you want to purchase from the above list");
-                var userInput = Console.ReadLine();
-                ProductTypes type = Enum.Parse<ProductTypes>(userInput.ToUpper());
-                if (type == ProductTypes.VIDEO)
-                {
-                    Console.WriteLine("Enter Video Name: ");
-                    itemName = Console.ReadLine();
-                }
-
-                var output = paymentController.HandlePayment(type, itemName);
-
-
+                Console.WriteLine("{0,3}   {1}",
+                    (int)value, ((ProductTypes)value));
             }
+
+            Console.WriteLine("Enter the Name of the product you want to purchase from the above list");
+            var userInput = Console.ReadLine();
+            ProductTypes type = Enum.Parse<ProductTypes>(userInput.ToUpper());
+            if (type == ProductTypes.VIDEO)
+            {
+                Console.WriteLine("Enter Video Name: ");
+                itemName = Console.ReadLine();
+            }
+
+            var output = paymentController.HandlePayment(type, itemName);
+
+            Console.WriteLine(output);
         }
     }
 }
